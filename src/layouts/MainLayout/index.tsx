89c20778908b810e18e8362e2b10332b329079c8
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2023-09-11 02:10:37 +0530

    add modal from headless ui

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { IoReorderThreeOutline } from "react-icons/io5";
 import { BsBell } from "react-icons/bs";
-import { FiEdit } from "react-icons/fi";
-import { signIn, useSession } from "next-auth/react";
+import { FiEdit, FiLogOut } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const MainLayout = ({ children }: React.PropsWithChildren) => {
   const { data: sessionData, status } = useSession();
@@ -24,10 +24,24 @@ const MainLayout = ({ children }: React.PropsWithChildren) => {
               <div className="h-5 w-5 rounded-full bg-gray-600"></div>
             </div>
             <div>
-              <button className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900">
+              <button
+                onClick={() => console.log("Write button")}
+                className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900"
+              >
                 <div>Write</div>
                 <div>
                   <FiEdit />
+                </div>
+              </button>
+            </div>
+            <div>
+              <button
+                onClick={() => signOut()}
+                className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900"
+              >
+                <div>LogOut</div>
+                <div>
+                  <FiLogOut />
                 </div>
               </button>
             </div>
