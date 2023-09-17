6ef42f0ec89d57fe9ec046f8770a62b3a88d8a34
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2023-09-18 04:31:21 +0530

    add comment to database

---
+++ src/components/CommentSidebar/index.tsx
@@ -0,0 +1,124 @@
+import React, { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { HiXMark } from "react-icons/hi2";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { trpc } from "../../utils/trpc";
+import { toast } from "react-hot-toast";
+
+type CommentSidebarProps = {
+  showCommentSidebar: boolean;
+  setShowCommentSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+  postId: string;
+};
+
+type CommentFormType = { text: string };
+
+export const CommentFormSchema = z.object({
+  text: z.string().min(3),
+});
+
+const CommentSidebar = ({
+  showCommentSidebar,
+  setShowCommentSidebar,
+  postId,
+}: CommentSidebarProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<CommentFormType>({
+    resolver: zodResolver(CommentFormSchema),
+  });
+
+  const submitComment = trpc.post.submitComment.useMutation({
+    onSuccess: () => {
+      toast.success("Comment added");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return (
+    <Transition.Root show={showCommentSidebar} as={Fragment}>
+      <Dialog as="div" onClose={() => setShowCommentSidebar(false)}>
+        <div className="fixed right-0 top-0">
+          <Transition.Child
+            enter="transition duration-1000"
+            leave="transition duration-500"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
+          >
+            <Dialog.Panel className="relative h-screen w-[200px] max-w-md bg-white shadow-md sm:w-[400px]">
+              <div className="flex h-full w-full flex-col overflow-scroll px-6">
+                <div className="my-10 mb-5 flex items-center justify-between text-xl">
+                  <h2 className="font-medium">Response (4)</h2>
+                  <div>
+                    <HiXMark
+                      className="cursor-pointer"
+                      onClick={() => setShowCommentSidebar(false)}
+                    />
+                  </div>
+                </div>
+
+                <form
+                  onSubmit={handleSubmit((data) => {
+                    submitComment.mutate({
+                      ...data,
+                      postId,
+                    });
+                  })}
+                  className="my-6 flex flex-col items-end space-y-5"
+                >
+                  <textarea
+                    id="comment"
+                    rows={3}
+                    className="w-full rounded-xl border border-gray-300 p-4 shadow-lg outline-none focus:border-gray-600"
+                    placeholder="What are your thoughts?"
+                    {...register("text")}
+                  />
+                  {isValid && (
+                    <button
+                      type="submit"
+                      className="flex items-center space-x-3 rounded border border-gray-300 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900"
+                    >
+                      Comment
+                    </button>
+                  )}
+                </form>
+
+                <div className="flex flex-col items-center justify-center space-y-6">
+                  {Array.from({ length: 4 }).map((_, i) => (
+                    <div
+                      className="flex w-full flex-col space-y-2 border-b border-b-gray-300 pb-4 last:border-none"
+                      key={i}
+                    >
+                      <div className="flex w-full items-center space-x-2 text-xs">
+                        <div className="relative h-8 w-8 rounded-full bg-gray-400"></div>
+                        <div>
+                          <p className="font-semibold">Shrinjoy Saha</p>
+                          <p>01-02-2020</p>
+                        </div>
+                      </div>
+                      <div className="text-sm text-gray-600">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Possimus labore dolore asperiores delectus corporis
+                        autem eos, laborum dignissimos voluptate dolorum!
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default CommentSidebar;
--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,12 +1,11 @@
 import { useRouter } from "next/router";
-import React, { Fragment, useCallback, useState } from "react";
+import React, { useCallback, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 import { trpc } from "../utils/trpc";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 import { BsChat } from "react-icons/bs";
-import { Dialog, Transition } from "@headlessui/react";
-import { HiXMark } from "react-icons/hi2";
+import CommentSidebar from "../components/CommentSidebar";
 
 const PostPage = () => {
   const router = useRouter();
@@ -41,72 +40,13 @@ const PostPage = () => {
 
   return (
     <MainLayout>
-      <Transition.Root show={showCommentSidebar} as={Fragment}>
-        <Dialog as="div" onClose={() => setShowCommentSidebar(false)}>
-          <div className="fixed right-0 top-0">
-            <Transition.Child
-              enter="transition duration-1000"
-              leave="transition duration-500"
-              enterFrom="translate-x-full"
-              enterTo="translate-x-0"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-full"
-            >
-              <Dialog.Panel className="relative h-screen w-[200px] max-w-md bg-white shadow-md sm:w-[400px]">
-                <div className="flex h-full w-full flex-col overflow-scroll px-6">
-                  <div className="my-10 mb-5 flex items-center justify-between text-xl">
-                    <h2 className="font-medium">Response (4)</h2>
-                    <div>
-                      <HiXMark
-                        className="cursor-pointer"
-                        onClick={() => setShowCommentSidebar(false)}
-                      />
-                    </div>
-                  </div>
-
-                  <form className="flex flex-col items-end space-y-5">
-                    <textarea
-                      id="comment"
-                      rows={3}
-                      className="w-full rounded-xl border border-gray-300 p-4 shadow-lg outline-none focus:border-gray-600"
-                      placeholder="What are your thoughts?"
-                    />
-                    <button
-                      type="submit"
-                      className="flex items-center space-x-3 rounded border border-gray-300 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900"
-                    >
-                      Comment
-                    </button>
-                  </form>
-
-                  <div className="flex flex-col items-center justify-center space-y-6">
-                    {Array.from({ length: 4 }).map((_, i) => (
-                      <div
-                        className="flex w-full flex-col space-y-2 border-b border-b-gray-300 pb-4 last:border-none"
-                        key={i}
-                      >
-                        <div className="flex w-full items-center space-x-2 text-xs">
-                          <div className="relative h-8 w-8 rounded-full bg-gray-400"></div>
-                          <div>
-                            <p className="font-semibold">Shrinjoy Saha</p>
-                            <p>01-02-2020</p>
-                          </div>
-                        </div>
-                        <div className="text-sm text-gray-600">
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Possimus labore dolore asperiores delectus
-                          corporis autem eos, laborum dignissimos voluptate
-                          dolorum!
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition.Root>
+      {getPost.data?.id && (
+        <CommentSidebar
+          showCommentSidebar={showCommentSidebar}
+          setShowCommentSidebar={setShowCommentSidebar}
+          postId={getPost.data?.id}
+        />
+      )}
 
       {getPost.isLoading && (
         <div className="flex h-full w-full items-center justify-center space-x-4">
