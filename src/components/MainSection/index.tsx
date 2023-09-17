335b9df810dd62b63f9a31d39f19806a14371670
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2023-09-17 21:17:01 +0530

    bug fixing in bookmark feature

--- src/components/MainSection/index.tsx
+++ src/components/MainSection/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { trpc } from "../../utils/trpc";
-import { CiBookmarkCheck, CiBookmarkPlus, CiSearch } from "react-icons/ci";
-import dayjs from "dayjs";
-import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
 import { HiChevronDown } from "react-icons/hi";
-import Link from "next/link";
+import Post from "../Post";
 
 const MainSection = () => {
   const getPosts = trpc.post.getPosts.useQuery();
-  const bookmarkPost = trpc.post.bookmarkPost.useMutation();
-  const removeBookmarkPost = trpc.post.removeBookmark.useMutation();
 
   return (
     <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
@@ -65,85 +61,7 @@ const MainSection = () => {
         )}
 
         {getPosts.isSuccess &&
-          getPosts.data.map((post) => (
-            <div
-              key={post.id}
-              className="group flex flex-col space-y-4 border-b border-gray-300 pb-8 last:border-none"
-            >
-              <div className="flex w-full items-center space-x-2">
-                <div className="relative h-10 w-10 rounded-full bg-gray-400">
-                  {post.author.image && (
-                    <Image
-                      src={post.author.image}
-                      fill
-                      alt={post.author.name ?? ""}
-                      className="rounded-full"
-                    />
-                  )}
-                </div>
-                <div>
-                  <p className="font-semibold">
-                    {post.author.name} &#x2022;
-                    <span className="mx-1">
-                      {dayjs(post.createdAt).format("DD/MM/YYYY")}
-                    </span>
-                  </p>
-                  <p className="text-sm">The Founder, Software Developer</p>
-                </div>
-              </div>
-              <Link
-                href={`/${post.slug}`}
-                className="grid w-full grid-cols-12 gap-4"
-              >
-                <div className="col-span-8 flex flex-col space-y-4">
-                  <p className="text-2xl font-bold text-gray-800 decoration-indigo-600 group-hover:underline">
-                    {post.title}
-                  </p>
-                  <p className="break-words text-sm text-gray-500">
-                    {post.description}
-                  </p>
-                </div>
-                <div className="col-span-4">
-                  <div className="h-full w-full transform rounded-xl bg-gray-300 transition duration-300 hover:scale-105 hover:shadow-xl"></div>
-                </div>
-              </Link>
-              <div>
-                <div className="flex w-full items-center justify-between space-x-4">
-                  <div className="flex items-center space-x-2">
-                    {Array.from({ length: 4 }).map((_, i) => (
-                      <div
-                        key={i}
-                        className="rounded-2xl bg-gray-200/50 px-5 py-3"
-                      >
-                        tag {i}
-                      </div>
-                    ))}
-                  </div>
-                  <div>
-                    {post.bookmarks && post.bookmarks.length > 0 ? (
-                      <CiBookmarkCheck
-                        className="cursor-pointer text-3xl text-indigo-600"
-                        onClick={() => {
-                          removeBookmarkPost.mutate({
-                            postId: post.id,
-                          });
-                        }}
-                      />
-                    ) : (
-                      <CiBookmarkPlus
-                        className="cursor-pointer text-3xl"
-                        onClick={() => {
-                          bookmarkPost.mutate({
-                            postId: post.id,
-                          });
-                        }}
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+          getPosts.data.map((post) => <Post key={post.id} {...post} />)}
       </div>
     </main>
   );
