d178f248d367832dfefe930b1c0e4c4fa7d43909
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2023-09-18 02:44:16 +0530

    comment section UI

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,15 @@
 import { useRouter } from "next/router";
-import React, { useCallback } from "react";
+import React, { Fragment, useCallback, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 import { trpc } from "../utils/trpc";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 import { BsChat } from "react-icons/bs";
+import { Dialog, Transition } from "@headlessui/react";
+import { HiXMark } from "react-icons/hi2";
 
 const PostPage = () => {
   const router = useRouter();
-
   const postRoute = trpc.useContext().post;
 
   const getPost = trpc.post.getPost.useQuery(
@@ -36,8 +37,77 @@ const PostPage = () => {
     },
   });
 
+  const [showCommentSidebar, setShowCommentSidebar] = useState(false);
+
   return (
     <MainLayout>
+      <Transition.Root show={showCommentSidebar} as={Fragment}>
+        <Dialog as="div" onClose={() => setShowCommentSidebar(false)}>
+          <div className="fixed right-0 top-0">
+            <Transition.Child
+              enter="transition duration-1000"
+              leave="transition duration-500"
+              enterFrom="translate-x-full"
+              enterTo="translate-x-0"
+              leaveFrom="translate-x-0"
+              leaveTo="translate-x-full"
+            >
+              <Dialog.Panel className="relative h-screen w-[200px] max-w-md bg-white shadow-md sm:w-[400px]">
+                <div className="flex h-full w-full flex-col overflow-scroll px-6">
+                  <div className="my-10 mb-5 flex items-center justify-between text-xl">
+                    <h2 className="font-medium">Response (4)</h2>
+                    <div>
+                      <HiXMark
+                        className="cursor-pointer"
+                        onClick={() => setShowCommentSidebar(false)}
+                      />
+                    </div>
+                  </div>
+
+                  <form className="flex flex-col items-end space-y-5">
+                    <textarea
+                      id="comment"
+                      rows={3}
+                      className="w-full rounded-xl border border-gray-300 p-4 shadow-lg outline-none focus:border-gray-600"
+                      placeholder="What are your thoughts?"
+                    />
+                    <button
+                      type="submit"
+                      className="flex items-center space-x-3 rounded border border-gray-300 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900"
+                    >
+                      Comment
+                    </button>
+                  </form>
+
+                  <div className="flex flex-col items-center justify-center space-y-6">
+                    {Array.from({ length: 4 }).map((_, i) => (
+                      <div
+                        className="flex w-full flex-col space-y-2 border-b border-b-gray-300 pb-4 last:border-none"
+                        key={i}
+                      >
+                        <div className="flex w-full items-center space-x-2 text-xs">
+                          <div className="relative h-8 w-8 rounded-full bg-gray-400"></div>
+                          <div>
+                            <p className="font-semibold">Shrinjoy Saha</p>
+                            <p>01-02-2020</p>
+                          </div>
+                        </div>
+                        <div className="text-sm text-gray-600">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Possimus labore dolore asperiores delectus
+                          corporis autem eos, laborum dignissimos voluptate
+                          dolorum!
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
       {getPost.isLoading && (
         <div className="flex h-full w-full items-center justify-center space-x-4">
           <div>
@@ -49,7 +119,7 @@ const PostPage = () => {
 
       {getPost.isSuccess && (
         <div className="fixed bottom-10 flex w-full items-center justify-center">
-          <div className="group flex items-center justify-center space-x-4 rounded-full border border-gray-400 bg-white px-6 py-3 transition duration-300 hover:border-gray-900">
+          <div className="group flex items-center justify-center space-x-4 rounded-full border border-gray-400 bg-white px-6 py-3 shadow-xl transition duration-300 hover:border-gray-900">
             <div className="border-r pr-4 transition duration-300 group-hover:border-gray-900">
               {getPost.data?.likes.length && getPost.data?.likes.length > 0 ? (
                 <FcLike
@@ -74,7 +144,10 @@ const PostPage = () => {
               )}
             </div>
             <div>
-              <BsChat className="text-base" />
+              <BsChat
+                className="cursor-pointer text-base"
+                onClick={() => setShowCommentSidebar(true)}
+              />
             </div>
           </div>
         </div>
