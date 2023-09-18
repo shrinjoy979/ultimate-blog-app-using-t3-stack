e2c0b693c801f132b02654adf4d1c928d7dbe574
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2023-09-19 03:56:52 +0530

    add posts in profile section and profile page bug fixing

--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -3,7 +3,10 @@ import MainLayout from "../../layouts/MainLayout";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
+import { SlShareAlt } from "react-icons/sl";
 import { BiEdit } from "react-icons/bi";
+import { toast } from "react-hot-toast";
+import Post from "../../components/Post";
 
 const UserProfilePage = () => {
   const router = useRouter();
@@ -17,38 +20,77 @@ const UserProfilePage = () => {
     }
   );
 
+  const userPosts = trpc.user.getUserPosts.useQuery(
+    {
+      username: router.query.username as string,
+    },
+    {
+      enabled: !!router.query.username,
+    }
+  );
+
   return (
     <MainLayout>
       <div className="flex h-full w-full items-center justify-center">
         <div className="my-10 flex h-full w-full flex-col items-center justify-center lg:max-w-screen-sm xl:max-w-screen-lg">
-          <div className="relative h-44 w-full rounded-3xl bg-gradient-to-r from-rose-100 to-teal-100">
-            <div className="absolute -bottom-10 left-12">
-              <div className="group relative h-28 w-28 cursor-pointer rounded-full border-2 border-white bg-gray-100">
-                <label
-                  htmlFor="avatarFile"
-                  className="absolute z-10 flex h-full w-full cursor-pointer items-center justify-center rounded-full transition group-hover:bg-black/40"
+          <div className="flex w-full flex-col rounded-3xl bg-white shadow-md">
+            <div className="relative h-44 w-full rounded-t-3xl bg-gradient-to-r from-rose-100 to-teal-100">
+              <div className="absolute -bottom-10 left-12">
+                <div className="group relative h-28 w-28 cursor-pointer rounded-full border-2 border-white bg-gray-100">
+                  <label
+                    htmlFor="avatarFile"
+                    className="absolute z-10 flex h-full w-full cursor-pointer items-center justify-center rounded-full transition group-hover:bg-black/40"
+                  >
+                    <BiEdit className="hidden text-3xl text-white group-hover:block" />
+                    <input
+                      type="file"
+                      name="avatarFile"
+                      id="avatarFile"
+                      className="sr-only"
+                      accept="image/*"
+                    />
+                  </label>
+                  {userProfile.data?.image && (
+                    <Image
+                      src={userProfile.data?.image}
+                      alt={userProfile.data?.name ?? ""}
+                      fill
+                      className="rounded-full"
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="ml-12 mt-10 flex flex-col space-y-0.5 rounded-b-3xl py-5">
+              <div className="text-2xl font-semibold text-gray-800">
+                {userProfile.data?.name}
+              </div>
+              <div className="text-gray-600">@{userProfile.data?.username}</div>
+              <div className="text-gray-600">
+                {userProfile.data?._count?.posts ?? 0} Posts
+              </div>
+              <div>
+                <button
+                  onClick={() => {
+                    navigator.clipboard.writeText(window.location.href);
+                    toast.success("Link copied");
+                  }}
+                  className="mt-2 flex transform items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900 active:scale-95"
                 >
-                  <BiEdit className="hidden text-3xl text-white group-hover:block" />
-                  <input
-                    type="file"
-                    name="avatarFile"
-                    id="avatarFile"
-                    className="sr-only"
-                    accept="image/*"
-                  />
-                </label>
-                {userProfile.data?.image && (
-                  <Image
-                    src={userProfile.data?.image}
-                    alt={userProfile.data?.name ?? ""}
-                    fill
-                    className="rounded-full"
-                  />
-                )}
+                  <div>Share</div>
+                  <div>
+                    <SlShareAlt />
+                  </div>
+                </button>
               </div>
             </div>
           </div>
-          <div></div>
+          <div className="my-10 w-full">
+            {userPosts.isSuccess &&
+              userPosts.data?.posts.map((post) => (
+                <Post {...post} key={post.id} />
+              ))}
+          </div>
         </div>
       </div>
     </MainLayout>
--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -18,6 +18,50 @@ export const userRouter = router({
           image: true,
           id: true,
           username: true,
+          _count: {
+            select: {
+              posts: true,
+            },
+          },
+        },
+      });
+    }),
+
+  getUserPosts: publicProcedure
+    .input(
+      z.object({
+        username: z.string(),
+      })
+    )
+    .query(async ({ ctx: { prisma, session }, input: { username } }) => {
+      return await prisma.user.findFirst({
+        where: {
+          username,
+        },
+        select: {
+          posts: {
+            select: {
+              id: true,
+              slug: true,
+              title: true,
+              description: true,
+              createdAt: true,
+              author: {
+                select: {
+                  name: true,
+                  username: true,
+                  image: true,
+                },
+              },
+              bookmarks: session?.user?.id
+                ? {
+                    where: {
+                      userId: session?.user?.id,
+                    },
+                  }
+                : false,
+            },
+          },
         },
       });
     }),
