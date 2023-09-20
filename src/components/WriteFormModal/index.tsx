7617635c3762340dfa9fb7243a1e5e6cbb3187c7
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2023-09-20 19:38:43 +0530

    add tags UI

--- src/components/WriteFormModal/index.tsx
+++ src/components/WriteFormModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Modal from "../Modal";
 import { GlobalContext } from "../../contexts/GlobalContextProvider";
 import { useForm } from "react-hook-form";
@@ -7,6 +7,8 @@ import { z } from "zod";
 import { trpc } from "../../utils/trpc";
 import toast from "react-hot-toast";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import TagsAutocompletion from "../TagsAutocompletion";
+import TagForm from "../TagForm";
 
 type WriteFormType = {
   title: string;
@@ -46,59 +48,81 @@ const WriteFormModal = () => {
     createPost.mutate(data);
   };
 
-  return (
-    <Modal isOpen={isWriteModalOpen} onClose={() => setIsWriteModalOpen(false)}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="relative flex flex-col items-center justify-center space-y-4"
-      >
-        {createPost.isLoading && (
-          <div className="absolute flex h-full w-full items-center justify-center">
-            <AiOutlineLoading3Quarters className="animate-spin" />
-          </div>
-        )}
+  const [isTagCreateModalOpen, setIsTagCreateModalOpen] = useState(false);
 
-        <input
-          type="text"
-          id="title"
-          className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
-          placeholder="Title of the blog"
-          {...register("title")}
+  return (
+    <>
+      <Modal
+        isOpen={isWriteModalOpen}
+        onClose={() => setIsWriteModalOpen(false)}
+      >
+        <TagForm
+          isOpen={isTagCreateModalOpen}
+          onClose={() => setIsTagCreateModalOpen(false)}
         />
-        <p className="w-full pb-2 text-left text-sm text-red-500">
-          {errors.title?.message}
-        </p>
-        <input
-          type="text"
-          id="shortDescription"
-          className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
-          placeholder="Short description about the blog"
-          {...register("description")}
-        />
-        <p className="w-full pb-2 text-left text-sm text-red-500">
-          {errors.description?.message}
-        </p>
-        <textarea
-          id="mainBody"
-          cols={10}
-          rows={10}
-          className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
-          placeholder="Blog main body....."
-          {...register("text")}
-        />
-        <p className="w-full pb-2 text-left text-sm text-red-500">
-          {errors.text?.message}
-        </p>
-        <div className="flex w-full justify-end">
+        <div className="my-4 flex w-full items-center space-x-4">
+          <div className="z-10 w-4/5">
+            <TagsAutocompletion />
+          </div>
           <button
-            type="submit"
-            className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900"
+            onClick={() => setIsTagCreateModalOpen(true)}
+            className="space-x-3 whitespace-nowrap rounded border border-gray-200 px-4 py-2 text-sm transition hover:border-gray-900 hover:text-gray-900"
           >
-            Publish
+            Create Tag
           </button>
         </div>
-      </form>
-    </Modal>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="relative flex flex-col items-center justify-center space-y-4"
+        >
+          {createPost.isLoading && (
+            <div className="absolute flex h-full w-full items-center justify-center">
+              <AiOutlineLoading3Quarters className="animate-spin" />
+            </div>
+          )}
+
+          <input
+            type="text"
+            id="title"
+            className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
+            placeholder="Title of the blog"
+            {...register("title")}
+          />
+          <p className="w-full pb-2 text-left text-sm text-red-500">
+            {errors.title?.message}
+          </p>
+          <input
+            type="text"
+            id="shortDescription"
+            className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
+            placeholder="Short description about the blog"
+            {...register("description")}
+          />
+          <p className="w-full pb-2 text-left text-sm text-red-500">
+            {errors.description?.message}
+          </p>
+          <textarea
+            id="mainBody"
+            cols={10}
+            rows={10}
+            className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
+            placeholder="Blog main body....."
+            {...register("text")}
+          />
+          <p className="w-full pb-2 text-left text-sm text-red-500">
+            {errors.text?.message}
+          </p>
+          <div className="flex w-full justify-end">
+            <button
+              type="submit"
+              className="flex items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900"
+            >
+              Publish
+            </button>
+          </div>
+        </form>
+      </Modal>
+    </>
   );
 };
 
